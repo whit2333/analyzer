b8cb915104de743fb2b85a8ff22d55da37fa88b3
Whitney Armstrong <warmstrong@anl.gov>
2019-02-09 15:52:13 -0500

    Moving off the farm 	modified:   Fadc250Module.h 	modified:   ../include/podd2/Logger.h 	modified:   ../src/podd2/src/Logger.cxx

--- src/podd2/src/Logger.cxx
+++ src/podd2/src/Logger.cxx
@@ -7,12 +7,19 @@ namespace fs = std::filesystem;
 #include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
 #endif
+#include <iostream>
 
 namespace podd2 {
 
   namespace log {
 
     std::shared_ptr<spdlog::logger> make_logger(std::string name, std::string file) {
+      fs::path log_file(file);
+      // the following creates /tmp/logs if default logs directory does  not exist.
+      fs::path log_dir( get_log_dir(log_file.parent_path()));
+
+      auto full_log_file = log_dir/log_file.filename();
+      
       std::vector<spdlog::sink_ptr> sinks;
       // ------------------------------------
       sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
@@ -22,7 +29,7 @@ namespace podd2 {
       sinks[0]->set_pattern("[%t] [%n] %^[%l]%$ %v");
       // ------------------------------------
       sinks.push_back(
-          std::make_shared<spdlog::sinks::basic_file_sink_mt>(file));
+          std::make_shared<spdlog::sinks::basic_file_sink_mt>(full_log_file.string()));
       sinks[1]->set_level(spdlog::level::trace);
       sinks[1]->set_pattern("[%t] [%n] %^[%l]%$ %v");
       // ------------------------------------
@@ -32,13 +39,24 @@ namespace podd2 {
       // ------------------------------------
       spdlog::register_logger(logger);
       //
-      fs::path log_file_path = file;
+      fs::path log_file_path = full_log_file;
       if (!fs::exists(log_file_path.parent_path())) {
         fs::create_directory(log_file_path.parent_path());
       }
       return logger;
     }
 
+    std::string get_log_dir(std::string dir)
+    {
+      fs::path dir_path(dir);
+      if( !fs::exists(dir_path)  ){
+        auto temp = fs::temp_directory_path() / dir_path;
+        std::cout << " creating tmp log file: " << temp.string() << "\n";
+        fs::create_directory(temp);
+        dir =  temp.string();
+      }
+      return dir;
+    }
   } // namespace log
 
 } // namespace podd2
